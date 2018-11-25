1e2d17908061e43d07a6bf2ba316e4d223bf461c
Senseytreyser <senseytreyser@mail.ru>
2018-11-25 21:51:40 +0300

    Сделаны 4 основные функции

---
+++ task_5.js
@@ -0,0 +1,14 @@
+//Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+
+function getSum(x,y){
+	return x + y; 
+}
+function getDif(x,y){
+	return x - y;
+}
+function getMult(x,y){
+	return x * y;
+}
+function getRatio(x,y){
+	return x / y;
+}
