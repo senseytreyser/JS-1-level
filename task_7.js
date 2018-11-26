b879c368cb4f0330202d805812fb1631e1e4daa4
Senseytreyser <senseytreyser@mail.ru>
2018-11-27 01:00:41 +0300

    Сравнил null и ноль...

--- task_7.js
+++ task_7.js
@@ -1,3 +1,8 @@
 // Сравнить null и 0. Попробуйте объяснить результат.
 
 console.log("Задание 7.\nСравнение null и 0");
+console.log("null > 0 "+(null > 0) ); // false
+console.log("null == 0 "+(null == 0) ); // false
+console.log("null >= 0 "+(null >= 0) ); // true
+
+//Объяснить это я затрудняюсь
